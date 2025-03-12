c2672350619a5e25a62cebf1c9cb9c4fce550450
Daniela Triana <101018203+danitriana@users.noreply.github.com>
2025-03-12 14:58:29 -0500

    docs: Agregar Pag

---
+++ js/youtube-widget.js
@@ -0,0 +1,7 @@
+(function() {
+    var tag = document.createElement("script");
+    tag.src = "https://apis.google.com/js/platform.js";
+    tag.async = true;
+    tag.defer = true;
+    document.head.appendChild(tag);
+})();
